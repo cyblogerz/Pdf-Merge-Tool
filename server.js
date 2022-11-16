2482ef3fd62abb314c61f5d215ec1b18de94a7e8
[cyblogerz] <[pranavajay2002@gmail.com]>
2022-11-16 15:26:54 +0530

    fixed merging

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const multer = require('multer')
 const upload = multer({dest: 'uploads/'})
+const {mergePDFs} = require('./merge')
 const app = express()
 const port = 8090
 
@@ -9,10 +10,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,'/templates/index.html'))
 })
 
+app.use('/static',express.static('public'))
 
-app.post('/merge', upload.array('pdfs',2),function (req, res,next){
+app.post('/merge', upload.array('pdfs',2),async (req, res,next) => {
   console.log(req.files);
-  res.send({data:req.files});
+  await mergePDFs(path.join(__dirname,req.files[0].path),path.join(__dirname,req.files[1].path));
+  res.redirect(`http://localhost:${port}/static/merged.pdf`);
   //multer - handle multiple file / form data
   
 })
